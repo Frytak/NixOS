10bcfc998212db0f897661e842fe5bc005e10779
Frytak <frytak2855@gmail.com>
2026-06-29 20:54:54 +0200

    Auto-commit: Mon 29 Jun 20:54:54 CEST 2026

--- rebuild.sh
+++ rebuild.sh
@@ -1,22 +1,13 @@
-echo -e "\x1b[1m\x1b[32mRebuilding NixOS configuration\x1b[0m"
-
-git add .
-git commit -m "$(date)"
-echo -e "\x1b[1m\x1b[32mCommited current configuration\x1b[0m"
-
-sudo nixos-rebuild switch --flake /etc/nixos/#BBM
-echo -e "\x1b[1m\x1b[32mSystem rebuilt!\x1b[0m"
-
 GREEN='\x1b[1;32m'
 RED='\x1b[1;31m'
 NC='\x1b[0m'
 
 cd /etc/nixos || {
-    echo -e "${RED}Failed to change directory to /etc/nixos${NC}"
+    echo -e "${RED}[Failed to change directory to /etc/nixos]${NC}"
     exit 1
 }
 
-echo -e "${GREEN}Rebuilding NixOS configuration...${NC}"
+echo -e "${GREEN}[Rebuilding NixOS configuration]${NC}"
 
 git add .
 
@@ -24,12 +15,12 @@ if git diff --staged --quiet; then
     echo "No new changes to commit."
 else
     git commit -m "Auto-commit: $(date)" > /dev/null
-    echo -e "${GREEN}Committed current configuration!${NC}"
+    echo -e "${GREEN}[Committed current configuration]${NC}"
 fi
 
 if sudo nixos-rebuild switch --flake /etc/nixos/\#BBM; then
-    echo -e "${GREEN}System rebuilt successfully!${NC}"
+    echo -e "${GREEN}[System rebuilt successfully]${NC}"
 else
-    echo -e "${RED}System rebuild failed! Please check the logs above.${NC}"
+    echo -e "${RED}[System rebuild failed]${NC}"
     exit 1 
 fi
