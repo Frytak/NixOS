4e3ce7426ab1369c72679a0892eb18a2e7dc07ad
Frytak <frytak2855@gmail.com>
2026-08-03 18:58:12 +0200

    Auto-commit: Mon  3 Aug 18:58:12 CEST 2026

--- hosts/BBM/default.nix
+++ hosts/BBM/default.nix
@@ -66,7 +66,7 @@
                 mods = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
                     Lithium = pkgs.fetchurl {
                         url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/f7vZ0VWU/lithium-fabric-0.25.3%2Bmc26.2.jar";
-                        sha512 = builtins.hashString "sha512" "text";
+                        hash = builtins.hashString "sha512" "text";
                     };
 
                     EasyAuth = pkgs.fetchurl {
