85524a0a8df698b58f1597d19098c0e7b730978e
Frytak <frytak2855@gmail.com>
2026-08-11 14:21:07 +0200

    Auto-commit: Tue 11 Aug 14:21:07 CEST 2026

--- hosts/Pavilion/home.nix
+++ hosts/Pavilion/home.nix
@@ -61,7 +61,7 @@
             hl.exec_cmd("uwsm app -- wayvnc")
         end)
 
-        hl.bind("M", hl.dsp.submap("kbptr"))
+        hl.bind("MOD + M", hl.dsp.submap("kbptr"))
 
         hl.define_submap("kbptr", function ()
             hl.bind("p", hl.dsp.exec_cmd("hyprctl dispatch submap reset && wl-kbptr && hyprctl dispatch submap kbptr"))
